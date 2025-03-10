b113d461f828988cdca223c5e4f106aa1377d871
mahanandhan <mahanandhanmanish@gmail.com>
2025-03-10 09:30:34 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,11 +5,11 @@ const App = () => {
   const data = [{
     "id": "1",
     "title": "web service",
-    "dis": "good for creating website projects"
+    "dis": "good for creating website projects using frontend and backend in frontend using basics: html css and js and react is advance and backend node.js express.js mongodb as database"
   }, {
     "id": "2",
     "title": "water service",
-    "dis": "this service provides water"
+    "dis": "this service provides water like giving water connections like drinking water and non-drinking water which helps people."
   }]
   return (
     <div>
